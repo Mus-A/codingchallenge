ec4e5261e289bc2581dc95da2024454b7afdc5b5
Mus A <Mustapha.afaar@gmail.com>
2022-04-02 22:58:11 +1100

    Initial Commit

---
+++ dev/main.js
@@ -0,0 +1,14 @@
+// let json = require('../products.json');
+// console.log(json, 'the json obj');
+
+const review = {
+    reviwerName: "",
+    body: "",
+    createdDate: "",
+    email: "",
+    productHandle: "",
+    productID: "",
+    imageURL: "",
+    isApproved: "",
+    
+}
